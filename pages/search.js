c09f962331f9f837cb4259439e67a2828ca9c584
thgee <thgeee@naver.com>
2025-01-25 20:58:36 +0900

    refactor: `Search페이지`를 SSR로 개선
    - `Search페이지`는 사용자가 입력한 query에 따라 페이지가 동적으로 생성되어야 하므로 SSG를 적용할 수 없습니다.
    - SEO 최적화를 위해 `CSR => SSR`로 변경하였습니다.

--- pages/search.js
+++ pages/search.js
@@ -1,37 +1,39 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 import ProductList from "@/components/ProductList";
 import SearchForm from "@/components/SearchForm";
 import axios from "@/lib/axios";
 import styles from "@/styles/Search.module.css";
-import Header from "@/components/Header";
-import Container from "@/components/Container";
 import Head from "next/head";
 
-export default function Search() {
-  const [products, setProducts] = useState([]);
-  const router = useRouter();
-  const { q } = router.query;
+export async function getServerSideProps(context) {
+  const { q } = context.query; // 쿼리에서 검색 키워드를 가져옴
+  let products = [];
 
-  async function getProducts(query) {
-    const res = await axios.get(`/products/?q=${query}`);
-    const nextProducts = res.data.results;
-    setProducts(nextProducts);
+  if (q) {
+    try {
+      const res = await axios.get(`/products/?q=${q}`);
+      products = res.data.results;
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
   }
 
-  useEffect(() => {
-    getProducts(q);
-    console.log(q);
-  }, [q]);
+  return {
+    props: {
+      products,
+      query: q || "",
+    },
+  };
+}
 
+export default function Search({ products, query }) {
   return (
     <div>
       <Head>
-        <title>{q} 검색 결과 - Codeitmall</title>
+        <title>{query} 검색 결과 - Codeitmall</title>
       </Head>
-      <SearchForm initialValue={q} />
+      <SearchForm initialValue={query} />
       <h2 className={styles.title}>
-        <span className={styles.keyword}>{q}</span> 검색 결과
+        <span className={styles.keyword}>{query}</span> 검색 결과
       </h2>
       <ProductList className={styles.productList} products={products} />
     </div>
